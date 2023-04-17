f56537c34bedc3df274e95944f567c8361d7fec1
jacquelinegroba <groba46@gmail.com>
2023-04-17 18:35:16 +0000

    Cambios

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     return (
       <> <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <Link to={"/"} className="navbar-brand">Nombre del E-commerce</Link>
+        <Link to={"/"} className="navbar-brand">Equipassion</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
